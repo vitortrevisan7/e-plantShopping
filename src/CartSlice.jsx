ecda4f6e0398107e307e7ea3e3e041a4e7b0a564
vitortrevisan7 <vitortnocente@gmail.com>
2025-05-09 18:22:40 -0300

    fff

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,36 +1,49 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-        const { name, image, cost } = action.payload; // Destructure product details from the action payload
-        // Check if the item already exists in the cart by comparing names
-        const existingItem = state.items.find(item => item.name === name);
-        if (existingItem) {
-            // If item already exists in the cart, increase its quantity
-            existingItem.quantity++;
-        } else {
-            // If item does not exist, add it to the cart with quantity 1
-            state.items.push({ name, image, cost, quantity: 1 });
-        }
+    name: 'cart',
+    initialState: {
+        items: [], // Initialize items as an empty array
+        numOfItems: 0 // Number of items multiplied by their quantity
     },
-    removeItem: (state, action) => {
-        const nameToRemove = action.payload.name;
-        state.items = state.items.filter(item => item.name !== nameToRemove);
+
+    reducers: {
+        addItem: (state, action) => {
+            const { name, image, cost } = action.payload;
+            const existingItem = state.items.find(item => item.name === name);
+
+            if (existingItem) {
+                // In existing items, quantity is already added as property
+                existingItem.quantity++;
+            } else {
+                state.items.push({ name, image, cost, quantity: 1 });
+            }
+
+            state.numOfItems += 1;
+        },
+
+        removeItem: (state, action) => {
+            const { name, quantity } = action.payload;
+            state.items = state.items.filter(item => item.name !== name);
+            state.numOfItems -= quantity;
+
+            // Just to be sure... I hate negative numbers
+            if (state.numOfItems < 0) {
+                state.numOfItems = 0;
+            }
+        },
+
+        updateQuantity: (state, action) => {
+            const { name, quantity } = action.payload;
+            const existingItem = state.items.find(item => item.name === name);
+
+            if (existingItem) {
+                const differenceQuantity = quantity - existingItem.quantity;
+                state.numOfItems += differenceQuantity;
+                existingItem.quantity = quantity;
+            }
+        },
     },
-    updateQuantity: (state, action) => {
-        const { name, quantity } = action.payload; // Destructure the product name and new quantity from the action payload
-        // Find the item in the cart that matches the given name
-        const itemToUpdate = state.items.find(item => item.name === name);
-        if (itemToUpdate) {
-        itemToUpdate.quantity = quantity; // If the item is found, update its quantity to the new value
-        }
-    },
-  },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
