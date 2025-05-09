f49e1da9573ea7892d86dd6cd4cec601cdbf4f39
vitortrevisan7 <vitortnocente@gmail.com>
2025-05-09 18:41:16 -0300

    sd

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -3,6 +3,7 @@ import './ProductList.css'
 import CartItem from './CartItem';
 import { useDispatch } from 'react-redux'; 
 import { addItem } from './CartSlice';     
+import { useSelector } from 'react-redux';
 
 function ProductList({ onHomeClick }) {
     const dispatch = useDispatch();
@@ -10,6 +11,7 @@ function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    
 
     const plantsArray = [
         {
@@ -267,6 +269,8 @@ function ProductList({ onHomeClick }) {
         }));
     };
 
+    const cartItemCount = useSelector(state => state.cart.numOfItems)
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -284,7 +288,24 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><div><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>{cartItemCount > 0 && (
+          <span style={{
+            position: 'absolute',
+            top: '-5px',
+            right: '-5px',
+            backgroundColor: 'red',
+            color: 'white',
+            borderRadius: '50%',
+            width: '25px',
+            height: '25px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontSize: '14px',
+          }}>
+            {cartItemCount}
+          </span>
+        )}</h1></div></a></div>
                 </div>
             </div>
             {!showCart ? (
